2284b94f597fe3d58cdd46939c55ef32c42b1294
Nayan Deshmukh <nayanapril2001@gmail.com>
2023-09-17 14:02:56 +0530

    new commit

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "nayan@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+accountEmail = "deshmukh@gmail.com";
+accountPassword = "98765";
+accountCity = "Jalna";
+
+// console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+let userName = "nayan";
+
+console.log(userName);
+
+// console.log(typeof userName);
+console.log(typeof userName);
+
+console.log(typeof null); // object
